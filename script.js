ad703738c5f0e77e65c2741665335d473ebeb4fa
gabriel <gabds.123@gmail.com>
2025-02-28 19:13:49 +0100

    change la maniere dont le code comunique avec myLibrary,change le style d'affichage

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ addBookToLibrary(" In Search of Lost Time","Marcel Proust",3000);
 const library = document.querySelector('#boite')
 
 function createLib(){
- 
+
+    console.table(myLibrary);
+    library.innerHTML = "";
+
     myLibrary.forEach(book => {
        
     const title = document.createElement('div');
@@ -29,11 +32,6 @@ function createLib(){
     const check = document.createElement('input');
     const removeBook = document.createElement('button');
 
-
-    
-//    const change = document.createElement('button');
-//    const Remove = document.createElement('button');
-
     title.classList.add("title");
     author.classList.add("author");
     pages.classList.add("pages");
@@ -41,27 +39,21 @@ function createLib(){
     check.classList.add("check");
     removeBook.classList.add("removeBook");
 
-
- //   change.classList.add("change");
- //   Remove.classList.add("Remove");
-
     title.innerText = "Title: " + book.title;
     author.innerText = "Author: " + book.author;
     pages.innerText = "Number of pages: " + book.pages;
     read.innerText = "Read: ";
     removeBook.innerText = "X";
 
-//    change.innerText = "Change"
-//    Remove.innerText = "Remove"
-
     check.type = "checkbox"
 
     removeBook.addEventListener("click", ()=>{
-        card.remove('title');
+        const card = removeBook.closest(".card");
+        const index = Array.from(library.children).indexOf(card);
+        myLibrary.splice(index,1);
+        createLib();
     })
 
- //   read.appendChild(change);
- //   read.appendChild(Remove);
     const card = document.createElement('div');
     card.classList.add("card");
     library.appendChild(card);
@@ -74,11 +66,8 @@ function createLib(){
 
 
     read.appendChild(check);
-
 })
 }
-
-createLib();
 
 const addBook = document.querySelector("#addBook");
 const modal = document.querySelector("[data-modal]");
@@ -94,45 +83,7 @@ newBookForm.addEventListener("submit",(e) =>{
     const bookPages = formData.get("book-pages");
 
     addBookToLibrary(bookTitle,bookAuthor,bookPages);
-
-    const title = document.createElement('div');
-    const author = document.createElement('div');
-    const pages = document.createElement('div');
-    const read = document.createElement('div');
-    const check = document.createElement('input');
-    const removeBook = document.createElement('button');
-
-    title.classList.add("title");
-    author.classList.add("author");
-    pages.classList.add("pages");
-    read.classList.add("read");
-    check.classList.add("check");
-    removeBook.classList.add("removeBook");
-
-
-    title.innerText = "Title: " + bookTitle;
-    author.innerText = "Author: " + bookAuthor;
-    pages.innerText = "Number of pages: " + bookPages;
-    read.innerText = "Read: ";
-    removeBook.innerText = "X";
-
-    check.type = "checkbox";
-
-    removeBook.addEventListener("click", ()=>{
-        card.remove('title');
-    })
-
-    const card = document.createElement('div');
-    card.classList.add("card");
-    library.appendChild(card);
-
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(read);
-    card.appendChild(removeBook);
-
-    read.appendChild(check);
+    createLib();
 
     newBookForm.reset();
     modal.close();
@@ -147,6 +98,6 @@ closeModal.addEventListener("click",()=>{
     modal.close();
 })
 
-
+createLib();
 
 
